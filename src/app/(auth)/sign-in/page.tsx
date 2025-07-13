49393fad21b121fc9b4dfcf0823065515b30b4a7
mahveen-fatima <mahveenfatima2224@gmail.com>
2025-07-13 21:54:27 +0530

    signin page
    Signed-off-by: mahveen-fatima <mahveenfatima2224@gmail.com>

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,127 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useState } from "react"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schema/signInSchema"
+import { signIn } from "next-auth/react"
 
 const page = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const router = useRouter()
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: ""
+    }
+  })
+
+  
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true)
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+
+    if(result?.error) {
+      if(result.error == "CredentialsSignin") {
+        toast.error("Sign in failed")
+        setIsSubmitting(false)
+      } else {
+        toast.error("Sign in error")
+        setIsSubmitting(false)
+      }
+    }
+
+    if(result?.url) {
+      router.replace("/dashboard")
+      setIsSubmitting(false)
+    }
+    
+  }
+
   return (
-    <div>sign in page</div>
+    <div className="flex justify-center items-center min-h-screen bg-stone-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-3xl mb-3">
+            <span className="block font-bold">Welcome back to</span>ASK ME ANYTHING
+          </h1>
+          <p className="mb-4">Sign in to start your ama adventure.</p>
+          <p></p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+          >
+          
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" 
+                    {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" 
+                    {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full bg-stone-950 hover:bg-neutral-900" type="submit" disabled={isSubmitting}>
+              {
+                isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                  </>
+                ) : ( "Sign in")
+              }
+            </Button>
+          </form>
+        </Form>
+
+        <div className="text-center mt-4">
+          <p>
+            Sign up if new here{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
